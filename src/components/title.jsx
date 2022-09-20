984e4d338a7046c42cba816aade8ffc8c83a23d3
Sergio Nieto <snieto10@gmail.com>
2022-09-20 14:50:14 -0400

    first committitle completed

---
+++ src/components/title.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./title.css";
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="title-box">
+        <h1 class="title">BERKSHIRE HATHAWAY</h1>
+      </div>
+    );
+  }
+}
+
+export default Title;
